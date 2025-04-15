f69ada5855d4afaa1afa6189cf1cd2db55239cc1
Msi <fokc2022@mail.ru>
2025-04-15 19:32:03 +0300

    lab7

--- src/main/java/com/springlabs/controller/HomeController.java
+++ src/main/java/com/springlabs/controller/HomeController.java
@@ -1,11 +1,10 @@
-package com.springlabs;
+package com.springlabs.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-
     @GetMapping("/")
     public String home() {
         return "index";
